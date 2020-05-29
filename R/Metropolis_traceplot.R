5727c018a2ade76fefc10101479fd314e11ba252
d73mwf <34099039+DomDF@users.noreply.github.com>
2020-05-29 21:23:52 +0100

    Update Metropolis_traceplot.R

--- R/Metropolis_traceplot.R
+++ R/Metropolis_traceplot.R
@@ -29,7 +29,7 @@ Metropolis_traceplot <- function(Metropolis_RW_dataframe, include_burn_in = FALS
            mapping = aes(x = iter, y = value))+
       geom_line(mapping = aes(col = as.factor(chain)), alpha = alpha)+
       geom_vline(xintercept = max((Metropolis_RW_dataframe %>% 
-                                     dplyr::filter(status == 'burn-in'))$iter))
+                                     dplyr::filter(status == 'burn-in'))$iter))+
     facet_wrap(facets = ~ parameter, ncol = 1)+
       theme_ddf_light() + labs(col = 'Chain') + theme(legend.title = element_text())
     
