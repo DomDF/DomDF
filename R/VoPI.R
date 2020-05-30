da47dada4841ddf7afdd39c8fb597fb1cc6aeab4
d73mwf <34099039+DomDF@users.noreply.github.com>
2020-05-30 20:54:56 +0100

    Updates to VoPI

--- R/VoPI.R
+++ R/VoPI.R
@@ -1,6 +1,6 @@
 #' VoPI
 #'
-#' A Crude Monte Carlo Estimation of the Value of Perfect Information for a One-Dimensional Decision Analysis Under Uncertainty Problem
+#' A Latin Hypercube (or Crude Monte Carlo) Estimation of the Value of Perfect Information for a One-Dimensional Decision Analysis Under Uncertainty Problem
 #' 
 #' @param prior_dist A string representing the prior distribution function, for example 'norm', or 'exp' - REQUIRED.
 #' @param prior_args A list containing the parameter values of the prior distribution, for example list(mean = 1, sd = 1), or list(rate = 1) - REQUIRED.
@@ -8,22 +8,38 @@
 #' @param lim_args A list containing the parameter values of the limit distribution function (that which has an associated cost if exceeded), for example list(mean = 1, sd = 1), or list(rate = 1) - REQUIRED.
 #' @param EC_lim_ex A numeric cost associated with exceeding the limit distribution - REQUIRED.
 #' @param n_samples The total number of samples from which an expected value of perfect information will be estimated - Defaults to 10^4.
-
+#' @param method The sampling method used in the calculation. 'lhs' is Latin Hypercube sampling and 'mc' is Monte Carlo sampling - Defaults to 'lhs'.
+#' @param VoPI_seed A random seed for reproduceable sampling. Enter NULL if this is not required. - Defaults to 1008.
+#' 
 #' @keywords
 #' @export
 #' @examples
 #' estimated_VoPI <- VoPI(prior_dist = 'exp', prior_args = list(rate = 3), lim_dist = 'norm', lim_args = list(mean = 3, sd = 1), EC_lim_ex = 1e5)
 
-VoPI <- function(prior_dist, prior_args, lim_dist, lim_args, EC_lim_ex, n_samples = 1e4){
+VoPI <- function(prior_dist, prior_args, lim_dist, lim_args, ExpCost_lim_ex, n_samples = 1e4, method = 'lhs', VoPI_seed = 1008){
 
-  require(tidyverse)
+  require(tidyverse); require(lhs)
   
   if(!grepl(pattern = 'beta|binom|cauchy|exp|gamma|lnorm|norm|unif|weibull', 
             x = prior_dist) == TRUE) stop('Please select a valid prior distribution')
   
   if(!grepl(pattern = 'beta|binom|cauchy|exp|gamma|lnorm|norm|unif|weibull', 
             x = lim_dist) == TRUE) stop('Please select a valid limit distribution')
-
+  
+  if (method != 'lhs' & method != 'mc') stop("Please enter a valid sampling method (currently, only 'lhs' and 'mc' are available")
+  
+  set.seed(seed = VoPI_seed)
+  
+  if (method == 'lhs'){
+    
+    probs <- lhs::randomLHS(n = n_samples, k = 1)
+    
+  } else if (method == 'mc') {
+    
+    probs <- runif(n = n_samples)
+    
+  }
+  
   process_args <- function(args){
     
     processed_args <- paste(as.numeric(args[1]))
@@ -43,25 +59,33 @@ VoPI <- function(prior_dist, prior_args, lim_dist, lim_args, EC_lim_ex, n_sample
   
   prior_args_proc <- process_args(args = prior_args); lim_args_proc <- process_args(args = lim_args)
   
-  prior_samples <- rlang::eval_tidy(str2lang(paste0('r', prior_dist, '(', n_samples, ',', prior_args_proc, ')'))) 
-  lim_samples <- rlang::eval_tidy(str2lang(paste0('r', lim_dist, '(', n_samples, ',', lim_args_proc, ')'))) 
+  samples_df <- data.frame(prior_samples = double(length = n_samples), lim_samples = double(length = n_samples))
   
-  EC_prior <- sum(prior_samples > lim_samples) * (EC_lim_ex / n_samples)
+  for (i in seq(from = 1, to = n_samples, by = 1)){
+    
+    samples_df$prior_samples[i] <- rlang::eval_tidy(str2lang(paste0('q', prior_dist, '(', probs[i], ',', prior_args_proc, ')')))
+    samples_df$lim_samples[i] <- rlang::eval_tidy(str2lang(paste0('q', lim_dist, '(', probs[i], ',', lim_args_proc, ')')))
+    
+  }
+
+  ExpCost_prior <- sum(samples_df$prior_samples > samples_df$lim_samples) * (ExpCost_lim_ex / n_samples)
   
-  VoPI_df <- data.frame(prior_samples, prob_lim_ex = double(length = length(prior_samples)), EC = double(length = length(prior_samples))) %>% 
-    dplyr::arrange(prior_samples)
+  VoPI_df <- data.frame(samples_df$prior_samples, prob_lim_ex = double(length = n_samples), ExpCost = double(length = n_samples)) %>% 
+    dplyr::arrange(samples_df$prior_samples)
   
   for(i in seq(from = 1, to = n_samples, by = 1)){
     
-    VoPI_df$prob_lim_ex[i] <- rlang::eval_tidy(str2lang(paste0('p', lim_dist, '(', VoPI_df$prior_samples[i], ',', lim_args_proc, ')')))
-    VoPI_df$EC[i] <- VoPI_df$prob_lim_ex[i] * (EC_lim_ex / n_samples)
+    VoPI_df$prob_lim_ex[i] <- rlang::eval_tidy(str2lang(paste0('p', lim_dist, '(', VoPI_df$samples_df.prior_samples[i], ',', lim_args_proc, ')')))
+    VoPI_df$ExpCost[i] <- VoPI_df$prob_lim_ex[i] * (ExpCost_lim_ex / n_samples)
     
   }
   
-  EC_prepost <- mean(VoPI_df$EC)
+  ExpCost_prepost <- mean(VoPI_df$ExpCost)
   
-  VoPI <- max(0, (EC_prior - EC_prepost))
+  Exp_VoPI <- max(0, (ExpCost_prior - ExpCost_prepost))
   
-  return(VoPI)
+  VoPI_df <- data.frame(ExpCost_prior, ExpCost_prepost, Exp_VoPI, sampling_method = method)
+  
+  return(VoPI_df)
 
 }
